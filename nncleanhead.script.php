4ef53f4bb0b1c971654d4e14d32e230814645a6e
Niels Nübel <n.nuebel@nn-medienagentur.de>
2015-03-18 22:58:07 +0100

    Copyright angepasst

--- nncleanhead.script.php
+++ nncleanhead.script.php
@@ -2,8 +2,8 @@
 /**
  * @version 1.0.0
  * @package NNCleanhead
- * @copyright Copyright (c) 2009 - 2015 Niels Nübel
- * @license GNU General Public License version 2 or later;
+ * @copyright 2015 Niels Nübel
+ * @license This software is licensed under the MIT license: http://opensource.org/licenses/MIT
  * @link http://www.niels-nuebel.de
  */
 
